adbdae98428ea842cfa6a98d1fed6b8945099bc1
somanath <somanathpadhi9@gmail.com>
2021-12-02 00:32:59 +0530

    Added data binding to recyclerview

--- app/src/main/java/com/android/retrofit/PostAdapter.kt
+++ app/src/main/java/com/android/retrofit/PostAdapter.kt
@@ -1,31 +1,35 @@
 package com.android.retrofit
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.android.retrofit.databinding.PostListItemBinding
 import com.android.retrofit.dto.PostDTO
-import kotlinx.android.synthetic.main.post_list_item.view.*
 
 
 class PostAdapter(private val list: List<PostDTO>) :
     RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.post_list_item, parent, false)
-        return PostViewHolder(view)
+        return PostViewHolder(
+            PostListItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-        holder.itemView.userID.text = list?.get(position)?.id.toString()
-        holder.itemView.postTitle.text = list?.get(position)?.title.toString()
-        holder.itemView.description.text = list?.get(position)?.body.toString()
+        holder.binding.userID.text = list?.get(position)?.id.toString()
+        holder.binding.postTitle.text = list?.get(position)?.title.toString()
+        holder.binding.description.text = list?.get(position)?.body.toString()
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    inner class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class PostViewHolder(val binding: PostListItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
 }
 
